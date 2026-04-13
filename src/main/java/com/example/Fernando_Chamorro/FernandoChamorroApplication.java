ea6008ab7fd7a05293287c2061cc7da381a2e9c1
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-04-13 10:20:30 -0400

    Agregado manejo de errores

---
+++ src/main/java/com/example/Fernando_Chamorro/FernandoChamorroApplication.java
@@ -0,0 +1,13 @@
+package com.example.Fernando_Chamorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FernandoChamorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FernandoChamorroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Fernando_Chamorro/FernandoChamorroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Fernando_Chamorro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FernandoChamorroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
